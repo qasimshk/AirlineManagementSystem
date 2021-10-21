7f3aaad01836728628386e113d71dc99a0287a84
Muhammad Qasim <ins_sheikh@hotmail.com>
2021-10-21 12:55:57 +0100

    tetsing pipeline

--- airline.management.api/Startup.cs
+++ airline.management.api/Startup.cs
@@ -23,7 +23,7 @@ namespace airline.management.api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -34,7 +34,7 @@ namespace airline.management.api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
