3aa38e4d4d6e6451f6256c7d81b47a51ef5b028d
Muhammad Qasim <ins_sheikh@hotmail.com>
2021-11-05 17:04:34 +0000

    One more use test project added

---
+++ airline.orchestrator.service.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace airline.orchestrator.service.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
